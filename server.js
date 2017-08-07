0c143ad517f6f654021b7d9531ad60a5eeaa9dfa
Ahmed Megahed <mega_amd@yahoo.com>
2017-08-07 18:28:57 +0300

    ee

--- server.js
+++ server.js
@@ -2,5 +2,8 @@
 var http = require("http");
 
 var server = http.createServer(function(req,res){ 
-    res.end("test"); 
+    res.write("<div> Hello </div>")
+    res.end("test w"); 
 });
+
+server.listen(3000);
