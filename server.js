7f52ccd9f3baf2f5ac9a10df1cb51f8ece3d44cc
Ahmed Megahed <mega_amd@yahoo.com>
2017-08-09 17:26:29 +0300

    bf controllers

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 
 var http = require("http");
+var express = require("express");
+var app = express();
 
-var server = http.createServer(function(req,res){ 
-    res.write("<div> Hello </div>")
-    res.end("test w"); 
+//var ejsEngine = require("ejs-locals");
+
+////set up the view engine
+//app.set("view engine","pug"); // pug view engine
+
+//app.engine("ejs",ejsEngine);//support master pages
+//app.set("view engine","ejs");// ejs view engine
+
+app.set("view engine","vash"); // vash view engine
+
+app.get("/",function(req, res){
+    //res.send("<h1> Hello  </h1>");
+    //res.render("pug/index",{title:"Express + Pug"});
+    //res.render("ejs/index",{title:"Express + ejs"});
+    res.render("index",{title:"Express + vash"});
 });
+
+app.get("/api/users",function(req,res){
+    res.set("Content-Type","application/json");
+    res.send({name:"ahmed", age:33});
+});
+
+var server = http.createServer(app);
 
 server.listen(3000);
